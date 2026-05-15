0ec0bdddf86ce14cfeed9e5c69986cb6f82c668b
insane <a84771426@gmail.com>
2026-05-15 00:30:09 -0300

    o que você alterou

---
+++ site-data.js
@@ -0,0 +1,12 @@
+window.SITE = {
+  razaoSocial: "JOSE ROBERTO DA SILVA DESPANCHANTE LTDA",
+  nomeFantasia: "Ze Roberto Despachante",
+  cnpj: "64.743.200/0001-17",
+  endereco:
+    "Avenida Henrique de Holanda, s/n - LJ 08 - Vitória de Santo Antão/PE - CEP: 55.602-000",
+  telefone: "(81) 95267-711",
+  email: "joserobertodespachante1@gmail.com",
+  dpo: "joserobertodespachante1@gmail.com",
+  dpoNome: "Jose Roberto da Silva",
+  ano: 2026,
+};
